ccd82f3d2a1b8ce3c5e3572c051a4807d24fe8a9
Mark Norman Francis <norm@cackhanded.net>
2015-08-07 12:27:53 +0100

    Add some information to celery processing
    This is nasty but, eh, it works.

--- apps/bookmarks/tasks.py
+++ apps/bookmarks/tasks.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 from __future__ import absolute_import
 from datetime import datetime
 
@@ -20,6 +22,7 @@ def update_bookmark_archive(pk):
 def update_site_favicon(domain, icon):
     from .models import Website
 
+    print '** Updating %s favicon with %s' % (domain, icon)
     site, created = Website.objects.get_or_create(domain = domain)
     site.update_favicon(icon)
 
@@ -28,28 +31,29 @@ def update_site_favicon(domain, icon):
 def pull_from_pinboard():
     from .models import Bookmark
 
-    if settings.PINBOARD_AUTH:
-        pb = Pinboard(settings.PINBOARD_AUTH)
-        recent = pb.posts.recent()
+    print '** Pulling new bookmarks from Pinboard'
+    pb = Pinboard(settings.PINBOARD_AUTH)
+    recent = pb.posts.recent()
 
-        for bookmark in recent['posts']:
-            timestamp = timezone.make_aware(
-                bookmark.time,
-                timezone.get_current_timezone()
-            )
+    for bookmark in recent['posts']:
+        timestamp = timezone.make_aware(
+            bookmark.time,
+            timezone.get_current_timezone()
+        )
 
-            db_bookmark, created = Bookmark.objects.get_or_create(
-                defaults = {
-                    'title': bookmark.description,
-                    'description': bookmark.extended,
-                    'date_added': timestamp,
-                    'origin': 2,
-                },
-                url = bookmark.url,
-            )
-            if created:
-                for tag in bookmark.tags:
-                    db_bookmark.tags.add(tag)
+        db_bookmark, created = Bookmark.objects.get_or_create(
+            defaults = {
+                'title': bookmark.description,
+                'description': bookmark.extended,
+                'date_added': timestamp,
+                'origin': 2,
+            },
+            url = bookmark.url,
+        )
+        if created:
+            print '-- New bookmark created [%d] %s' % (db_bookmark.pk, db_bookmark)
+            for tag in bookmark.tags:
+                db_bookmark.tags.add(tag)
 
 
 @shared_task
@@ -57,6 +61,8 @@ def push_to_pinboard(pk):
     from .models import Bookmark
 
     bookmark = Bookmark.objects.get(pk=pk)
+
+    print '** Pushing bookmark to Pinboard — [%d] %s' % (bookmark.pk, bookmark)
     pb = Pinboard(settings.PINBOARD_AUTH)
     pb.posts.add(
         url = bookmark.url,
